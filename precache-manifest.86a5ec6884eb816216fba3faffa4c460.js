5df38d29f1a677d11ed5c8dc48de6de72254f5b2
Amanda Chawinska <amanda.chawinska@spoko.pl>
2020-09-27 19:20:24 +0200

    Updates

---
+++ precache-manifest.86a5ec6884eb816216fba3faffa4c460.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3dae36af3f05ce076670ae6958b47fe",
+    "url": "/pacman/index.html"
+  },
+  {
+    "revision": "e5d8512de72f772df29a",
+    "url": "/pacman/static/css/main.1f355f30.chunk.css"
+  },
+  {
+    "revision": "9d118e267794fb82ebc7",
+    "url": "/pacman/static/js/2.cb3068d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pacman/static/js/2.cb3068d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5d8512de72f772df29a",
+    "url": "/pacman/static/js/main.ce47b9f2.chunk.js"
+  },
+  {
+    "revision": "802522991228d2e9abc8",
+    "url": "/pacman/static/js/runtime-main.31137a62.js"
+  },
+  {
+    "revision": "c504eec39be72b7da2e814f70e7b3e66",
+    "url": "/pacman/static/media/ghost.c504eec3.svg"
+  },
+  {
+    "revision": "03edcd8c84e8e3661ef8d5819d2f0ec4",
+    "url": "/pacman/static/media/pacman.03edcd8c.svg"
+  }
+]);
